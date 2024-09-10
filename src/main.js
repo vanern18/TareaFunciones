1f9e4c5341b69c3f7fe2163c67935586db436c5d
Nombre prueba <prueba@ejemplo.com>
2024-09-10 18:56:08 +0200

    FinTarea

--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
-import "./style.css";
+// function cambiarNumero(){
+//     var numeroElemento=document.getElementById("numero");
+//     var numero=parseInt(numeroElemento.textContent);
+//     numeroElemento.textContent=numero +1;
 
-console.log("Hello from main");
+// }
+
+const boton = document.getElementById("incrementar");
+const numero = document.getElementById("numero-turno");
+let contador = 0;
+
+boton.onclick = function () {
+  contador++;
+  if (contador < 10) {
+    numero.textContent = "0" + contador;
+  } else {
+    numero.textContent = contador;
+  }
+  if (contador > 99) {
+    numero.textContent = "00";
+    contador = 0;
+  }
+};
+
+const botonDecrementar = document.getElementById("decrementarBtn");
+
+botonDecrementar.onclick = function () {
+  contador--;
+  if (contador < 10) {
+    numero.textContent = "0" + contador;
+  } else {
+    numero.textContent = contador;
+  }
+  if (contador < 0) {
+    contador = 0;
+    numero.textContent = "00";
+  }
+};
+const botonResetear = document.getElementById("resetear");
+botonResetear.onclick = function () {
+  contador = 0;
+  numero.textContent = "00";
+};
+
+const botonCambiarTurno = document.getElementById("cambiarTurno");
+botonCambiarTurno.onclick = function () {
+  const cambiarTurno = document.getElementById("turnoInput").value;
+  contador = cambiarTurno;
+  if (contador < 10) {
+    numero.textContent = "0" + contador;
+  } else {
+    numero.textContent = contador;
+  }
+};
